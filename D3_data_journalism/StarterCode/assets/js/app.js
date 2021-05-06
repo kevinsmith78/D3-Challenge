15957cd1b29138c92033ffd10cc449b971651023
Kevin Smith <kevin.ar.smith@gmail.com>
2021-05-06 15:41:14 -0500

    testing

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -68,6 +68,7 @@ var yLinearScale = d3.scaleLinear()
       .offset([80, -60])
       .html(function(d) {
         return (`${d.abbr}<br>Hair length: ${d.age}<br>Hits: ${d.obesity}`);
+      });
 
 // Step 7: Create tooltip in the chart
     chartGroup.call(toolTip);
@@ -75,12 +76,26 @@ var yLinearScale = d3.scaleLinear()
 // Step 8: Create event listeners to display and hide the tooltip
     circlesGroup.on("click", function(data) {
       toolTip.show(data, this);
-    });
+    })
 
     // onmouseout event
-    .on("mouseout", function(data, index) {
+      .on("mouseout", function(data, index) {
       toolTip.hide(data);
     });
 
+ // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Number of Billboard 100 Hits");
 
-
+      chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Hair Metal Band Hair Length (inches)");
+  }).catch(function(error) {
+    //console.log(error);
+  });
