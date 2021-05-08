6a51ace4f393108da784a3b76f0412628c988f6f
Kevin Smith <kevin.ar.smith@gmail.com>
2021-05-08 08:24:31 -0500

    minute

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -10,92 +10,92 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3
-    .select(".chart")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 //Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Import Data from an external CSV file 
-d3.csv("assets/data/data.csv").then(function(Data) {
-    //console.log(Data);
-    //console.log([Data]);
+d3.csv("assets/data/data.csv").then(function(weightData) {
+  //console.log(Data);
+  //console.log([Data]);
 
-    //Step 1. Parse Data/Cast as members (age/obesity)
-    weightData.forEach(function(Data) {
-        Data.age = +Data.age;
-        Data.obesity = +Data.obesity;
+  //Step 1. Parse Data/Cast as members (age/obesity)
+  weightData.forEach(function(Data) {
+    Data.age = +Data.age;
+    Data.obesity = +Data.obesity;
+  });
+
+  //Step.2 Create Scale Functions
+  var xLinearScale = d3.scaleLinear()
+    .domain([10, d3.max(weightData, d => d.age)])
+    .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(weightData, d => d.obesity)])
+    .range([height, 0]);
+
+  // Step 3: Create axis functions
+
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Step 4: Append Axes to the chart
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // Step 5: Create Circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(weightData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "15")
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+  // Step 6: Initialize tool tip   
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function (d) {
+      return (`${d.state}<br>Age %: ${d.age}<br>Weight %: ${d.obesity}`);
     });
 
-//Step.2 Create Scale Functions
-    var xLinearScale = d3.scaleLinear()
-        .domain([20, d3.max(Data, d => d.age)])
-        .range([0, width]);
+  // Step 7: Create tooltip in the chart
+  chartGroup.call(toolTip);
 
-var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(Data, d => d.obesity)])
-      .range([height, 0]);
-
-// Step 3: Create axis functions
-
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-
-// Step 4: Append Axes to the chart
-    chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
-      
-    chartGroup.append("g")
-      .call(leftAxis);
-
-// Step 5: Create Circles
-    var circlesGroup = chartGroup.selectAll("circle")
-        .data(Data)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xLinearScale(d.age))
-        .attr("cy", d => yLinearScale(d.obesity))
-        .attr("r", "15")
-        .attr("fill", "pink")
-        .attr("opacity", ".5");
-
-// Step 6: Initialize tool tip   
-    var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`${d.abbr}<br>Hair length: ${d.age}<br>Hits: ${d.obesity}`);
-      });
-
-// Step 7: Create tooltip in the chart
-    chartGroup.call(toolTip);
-
-// Step 8: Create event listeners to display and hide the tooltip
-    circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
-    })
+  // Step 8: Create event listeners to display and hide the tooltip
+  circlesGroup.on("click", function (Data) {
+    toolTip.show(Data, this);
+  })
 
     // onmouseout event
-      .on("mouseout", function(data, index) {
-      toolTip.hide(data);
+    .on("mouseout", function (Data, index) {
+      toolTip.hide(Data);
     });
 
- // Create axes labels
-    chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+  // Create axes labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Number of Billboard 100 Hits");
 
-      chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
-  }).catch(function(error) {
-    //console.log(error);
-  });
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Hair Metal Band Hair Length (inches)");
+}).catch(function (error) {
+  //console.log(error);
+});
